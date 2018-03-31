a300ab2752c0858e6fc511c9c06a1f690e26a442
Daniel Andrade <dca650@gmail.com>
2018-03-30 21:02:47 -0300

    Add support for Portuguese (POR)

--- mathparse/mathwords.py
+++ mathparse/mathwords.py
@@ -313,6 +313,58 @@ MATH_WORDS = {
             'миллиард': 1000000000,
             'триллион': 1000000000000
         }
+    },
+    'POR': {
+        'unary_operators': {
+            'ao quadrado': '^ 2',
+            'ao cubo': '^ 3',
+            'raiz quadrada de': 'sqrt'
+        },
+        'binary_operators': {
+            'mais': '+',
+            'dividido por': '/',
+            'menos': '-',
+            'vezes': '*',
+            'elevado à potência de': '^'
+        },
+        'numbers': {
+            'zero': 0,
+            'um': 1,
+            'dois': 2,
+            'três': 3,
+            'quatro': 4,
+            'cinco': 5,
+            'seis': 6,
+            'sete': 7,
+            'oito': 8,
+            'nove': 9,
+            'dez': 10,
+            'onze': 11,
+            'doze': 12,
+            'treze': 13,
+            'quatorze': 14,
+            'catorze': 14,
+            'quinze': 15,
+            'dezesseis': 16,
+            'dezessete': 17,
+            'dezoito': 18,
+            'dezenove': 19,
+            'vinte': 20,
+            'trinta': 30,
+            'quarenta': 40,
+            'cinquenta': 50,
+            'sessenta': 60,
+            'setenta': 70,
+            'oitenta': 80,
+            'noventa': 90
+        },
+        'scales': {
+            'cem': 100,
+            'mil': 1000,
+            'milhão': 1000000,
+            'bilhão': 1000000000,
+            'trilhão': 1000000000000
+        }
     }
 }
 
