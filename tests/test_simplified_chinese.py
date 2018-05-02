baa3a7e78cc4fbc3d530196317d07a5a1ded54e2
12ycli <934345073@qq.com>
2018-05-02 21:28:05 +0800

    support simplified Chinese to calc and add test

---
+++ tests/test_simplified_chinese.py
@@ -0,0 +1,50 @@
+from unittest import TestCase
+from mathparse import mathparse
+
+
+class SimplifiedChineseTestCase(TestCase):
+
+    def test_addition_words(self):
+        result = mathparse.parse('四加四',language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 8)
+
+    def test_addition_words_large(self):
+        result = mathparse.parse('四千两百一加五百', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 4701)
+
+    def test_subtraction_words(self):
+        result = mathparse.parse('三十减二十',language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 10)
+
+    def test_multiplication_words(self):
+        result = mathparse.parse('九乘九', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 81)
+
+    def test_division_words(self):
+        result = mathparse.parse('十五除以五', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 3)
+
+    def test_division_words_large(self):
+        result = mathparse.parse('一千两百四除以一百', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(str(result), '12.04')
+
+    def test_sqrt(self):
+        result = mathparse.parse('四开方', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 2)
+
+    def test_squared(self):
+        result = mathparse.parse('二平方', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 4)
+
+    def test_cubed(self):
+        result = mathparse.parse('二立方', language='SIMPLIFIED_CHINESE')
+
+        self.assertEqual(result, 8)
