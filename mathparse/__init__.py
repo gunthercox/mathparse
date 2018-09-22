609b9f1de37befd91007dbeff22e0d0f31a3c88c
Gunther Cox <gunthercx@gmail.com>
2018-09-22 11:56:27 -0400

    Update version to 0.1.2

--- mathparse/__init__.py
+++ mathparse/__init__.py
@@ -2,7 +2,7 @@
 mathparse is a library for solving mathematical equations contained in strings
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
 __url__ = 'https://github.com/gunthercox/mathparse'
